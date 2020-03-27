218b9d096ab049e1ed96eabb8f271426202dd989
christian1894 <green_pok@hotmail.com>
2020-03-26 21:28:56 -0500

    Assesment

--- puerto-rico-mortality.R
+++ puerto-rico-mortality.R
@@ -4,6 +4,9 @@ system("cmd.exe", input = paste("start", fn))
 
 library(pdftools)
 library(stringr)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
 
 txt = pdf_text(fn)
 x = str_split(txt[9], '\n')
@@ -22,8 +25,26 @@ header[3]
 total_index = str_which(s, "Total")
 total_index
 
+
 n = str_count(s, "\\d+")
 sum(n == 1)
-out <- c(1:header_index, which(n==1), tail_index:length(s))
+out <- c(1:header_index, which(n==1), total_index:length(s))
 s <- s[-out]
 length(s)
+s <- str_remove_all(s, "[^\\d\\s]")
+
+s <- str_split_fixed(s, "\\s+", n = 6)[,1:5]
+colnames(s) = c("day", header)
+s = apply(s, 2, as.numeric)
+s = as.data.frame(s)
+mean(s$`2015`)
+mean(s$`2016`)
+mean(s$`2017`[1:19])
+mean(s$`2017`[20:30])
+
+s <- subset( s, select = -`2018` )
+
+s <- s %>% gather(year, deaths, -day) %>%
+  mutate(deaths = as.numeric(deaths))
+
+s %>% ggplot(aes(day, deaths)) + geom_point(aes(color = year))
